80c472c0243f2b98361031b237e187fd760b679c
TechnoDiktator <rastogitarang5@gmail.com>
2026-07-12 20:10:06 +0530

    adding service layer

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,54 @@
+package com.pm.patientservice.dto;
+
+public class PatientResponseDTO {
+
+    private String id;
+    private String address;
+    private String dateOfBirth;
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+
+}
