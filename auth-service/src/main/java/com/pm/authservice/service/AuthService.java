230f401f8c4974abaff8e1cbe0fb88f1e6d793ba
TechnoDiktator <rastogitarang5@gmail.com>
2026-07-18 22:40:44 +0530

    auth service

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -2,6 +2,7 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
+import com.pm.authservice.utility.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +12,19 @@ import java.util.Optional;
 public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
-
+    private final JwtUtil jwtUtil;
     private final UserService userService;
 
-    public AuthService(UserService userService , PasswordEncoder passwordEncoder){
+    public AuthService(UserService userService , PasswordEncoder passwordEncoder , JwtUtil jwtUtil){
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
 
     }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
         Optional<String> token = userService.findByEmail(loginRequestDTO.getWmail())
-                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword()))
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword() , u.getPassword()))
                 .map(u -> jwtUtil.generateToken(u.getEmail() , u.getRole()));
         return token;
 
